1c242725f61aa0ab19afd51ca78a12087335f6b6
maDU59_ <matthias.ducourant@gmail.com>
2026-09-03 09:43:49 +0200

    Refactored

--- src/main/java/fr/madu59/MoreParticleEffects.java
+++ src/main/java/fr/madu59/MoreParticleEffects.java
@@ -1,4 +1,4 @@
-package com.example;
+package fr.madu59;
 
 import net.fabricmc.api.ModInitializer;
 
@@ -7,8 +7,8 @@ import net.minecraft.resources.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
-	public static final String MOD_ID = "modid";
+public class MoreParticleEffects implements ModInitializer {
+	public static final String MOD_ID = "moreparticleeffects";
 
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
@@ -20,8 +20,6 @@ public class ExampleMod implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
 	}
 
 	public static Identifier id(String path) {
